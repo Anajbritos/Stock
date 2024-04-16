886a99d3149c0d89dbe3ceaace70cfca77692bbc
“Ana <anajbritos@gmail.com>
2024-04-16 15:50:43 -0300

    Configurado docker-compose do projeto

---
+++ src/main/java/io/ana/julia/stock/stockApplication.java
@@ -0,0 +1,13 @@
+package io.ana.julia.stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class stockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(stockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/ana/julia/stock/stockApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ana.julia.stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class stockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
